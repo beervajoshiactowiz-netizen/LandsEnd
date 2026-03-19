1b2fce5a12a65b336c3531642aeb95bec9f7e382
beervajoshiactowiz-netizen <beervajoshi.actowiz@gmail.com>
2026-03-19 11:26:51 +0530

    add comments, and keeping empty dict in loop, to get distinct values

--- parser.py
+++ parser.py
@@ -14,16 +14,17 @@ def parser(text):
     prodUrl=sel.jmespath('pageMetaOverride.openGraphTags[2].content').get()
     brand=sel.jmespath('productDetail.brandName').get()
     variants=[] #variants list
-    info = {}
+
     for sku in sel.jmespath('productDetail.skus'):
+        info = {}
         info['size_code']=sku.jmespath('sizeCode').get()
         info['colorCode']=sku.jmespath('colorCode').get()
         info["sale_price"]=sku.jmespath('price.currentPrice').get()
         info["original_price"]=sku.jmespath('price.originalPrice').get()
         info["size"]=sku.jmespath('size.values[0].label').get()
         info["size_label"] = sku.jmespath('size.values[0].longLabel').get()
-        info["size_range"] = sku.jmespath('attributesTypes[0].values[0].label').get()
-        info["fit"] = sku.jmespath('attributesTypes[1].values[0].label').get()
+        info["size_range"] = sku.jmespath('attributeTypes[0].values[0].label').get()
+        info["fit"] = sku.jmespath('attributeTypes[1].values[0].label').get()
         info["color"]=sku.jmespath('color.values[0].label').get()
         variants.append(info)
 
